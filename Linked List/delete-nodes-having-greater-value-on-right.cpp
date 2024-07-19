8c73a94400ffbbca99af84d89df47d95d4e12ef7
sivaprakash1603 <130375529+sivaprakash1603@users.noreply.github.com>
2024-07-19 17:11:09 +0530

    Create delete-nodes-having-greater-value-on-right.cpp

---
+++ Linked List/delete-nodes-having-greater-value-on-right.cpp
@@ -0,0 +1,54 @@
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    
+    public:
+
+    Node *compute(Node *head)
+    {
+        if(head==NULL||head->next==NULL){
+            return head;
+        }
+        Node* prev=NULL;
+        Node* curr=head;
+        while(curr!=NULL){
+            Node* temp=curr->next;
+            while(temp!=NULL&&temp->data<=curr->data){
+                temp=temp->next;
+            }
+            if(temp==NULL){
+                prev=curr;
+                curr=curr->next;
+            }
+            else if(temp!=NULL&&curr==head) {
+                curr=curr->next;
+                head=curr;
+            }
+            else if(temp!=NULL&&curr!=head){
+                prev->next=curr->next;
+                curr=prev->next;
+            }
+                    
+
+        }
+        return head;
+    }
+        
+    
+    
+};
+   
