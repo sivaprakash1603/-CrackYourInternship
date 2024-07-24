f8e00b5c2162b10b6fa3daf4586c265bdfb75ef6
sivaprakash1603 <130375529+sivaprakash1603@users.noreply.github.com>
2024-07-25 00:01:18 +0530

    Create find-the-duplicate-number.cpp

---
+++ Arrays/find-the-duplicate-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+    vector<int> a(nums.size(),0);
+    for(int i=0;i<nums.size();i++){
+    if((++a[nums[i]])>1) return nums[i];
+    }
+    return -1;    
+    }
+};
