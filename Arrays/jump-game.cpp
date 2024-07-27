f5c337759dd472ecac56152dd106bd56e50a1004
sivaprakash1603 <130375529+sivaprakash1603@users.noreply.github.com>
2024-07-27 14:34:46 +0530

    Create jump-game.cpp

---
+++ Arrays/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxJump = 0;
+        int n=nums.size()-1;
+        for(int i=0;i<=n;i++){
+            if(i>maxJump)
+                return false;
+            maxJump = max(nums[i]+i, maxJump);
+        }
+        return true;
+    }
+};
